c5d1a670d38bc9f91355f3c98aef703eaa3320c5
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-07-24 18:16:11 -0300

    Adjusted fourth page
    Adjusted fourth page

--- Scripts/AuxiliaryTools/AuxiliaryTool.js
+++ Scripts/AuxiliaryTools/AuxiliaryTool.js
@@ -11,3 +11,23 @@ function toggleContent(element, arrow) {
         arrow.classList.remove("ArrowToDown");
     }
 }
+
+const widthThreshold = 425;
+
+function replaceClassesIfNeeded() {
+  if (window.innerWidth <= widthThreshold) {
+    document.querySelectorAll('.expanded').forEach(element => {
+      element.classList.remove('expanded');
+      element.classList.add('collapsed');
+    });
+
+    document.querySelectorAll('.ArrowToRightNoExpanded').forEach(element => {
+      element.classList.remove('ArrowToRightNoExpanded');
+      element.classList.add('ArrowToRight');
+    });
+  }
+}
+
+replaceClassesIfNeeded();
+
+window.addEventListener('resize', replaceClassesIfNeeded);
